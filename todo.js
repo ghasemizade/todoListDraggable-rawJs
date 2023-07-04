ab22a0481cd603fe267b187c4dea2b372d7101c5
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-07-04 20:15:06 +0330

    update todo list js

--- todo.js
+++ todo.js
@@ -3,6 +3,11 @@ const getContainer = document.getElementById('container')
 const getModal = document.getElementById('modal')
 const getOverlay = document.getElementById('overlay')
 const getCancle = document.getElementById('cancle')
+const getClose = document.querySelector('.task')
+const getCloseIcon = document.getElementById('close-icon')
+const getInput = document.getElementById('input')
+const getAddBtn = document.getElementById('btn-add')
+const getStatusContent = document.getElementById('status-content')
 
 function modalHandler() {
     // getContainer.style.display = 'none'
@@ -10,12 +15,38 @@ function modalHandler() {
     getOverlay.style.display = 'flex'
     // getContainer.style.
 }
-function modalcancle() {
+function modalCancle() {
     getContainer.style.display = 'flex'
     getModal.style.display = 'none'
     getOverlay.style.display = 'none'
     
 }
 
+// getAddBtn.addEventListener('click', function () {
+//     console.log(getInput.value)
+// })
+function elementTaskHandler() {
+    let createDivElem = document.createElement('div')
+    let createPElem = document.createElement('p')
+    let createIconElem = document.createElement('ion-icon')
+    createDivElem.className = 'task'
+    createDivElem.draggable = 'true'
+    createPElem.id = 'desc'
+    createPElem.innerHTML = getInput.value
+    createIconElem.id = 'close-icon'
+    createIconElem.setAttribute('name', 'close-outline')
+    getStatusContent.appendChild(createDivElem)
+    createDivElem.appendChild(createPElem)
+    createDivElem.appendChild(createIconElem)
+    
+}
+function deleteIcon() {
+    getClose.remove()
+}
+
+console.log(getInput)
+
 getAddTodoBtn.addEventListener('click', modalHandler)
-getCancle.addEventListener('click', modalcancle)
+getCancle.addEventListener('click', modalCancle)
+getCloseIcon.addEventListener('click', deleteIcon)
+getAddBtn.addEventListener('click', elementTaskHandler)
